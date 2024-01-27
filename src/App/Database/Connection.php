c91150db8507bef9ac334ea48c45df029e4e8b2c
Hasmukh K Mistry <hasmukhmistry137@gmail.com>
2024-01-27 21:56:17 +0530

    Refactor Connection class to include PDO instance and connection management

--- src/App/Database/Connection.php
+++ src/App/Database/Connection.php
@@ -6,19 +6,42 @@ use PDO;
 
 class Connection
 {
+    /**
+     * PDO instance
+     *
+     * @var mixed
+     */
     private $pdo;
 
+    /**
+     * Create a new Connection instance
+     *
+     * @param string $host     Database host
+     * @param string $dbname   Database name
+     * @param string $user     Database user
+     * @param string $password Database password
+     */
     public function __construct($host, $dbname, $user, $password)
     {
         $dsn = "mysql:host=$host;dbname=$dbname";
         $this->pdo = new PDO($dsn, $user, $password);
     }
 
-    public function getPdo()
+    /**
+     * Get the PDO instance
+     *
+     * @return mixed
+     */
+    public function getPdo(): mixed
     {
         return $this->pdo;
     }
 
+    /**
+     * Close the PDO connection
+     *
+     * @return void
+     */
     public function __destruct()
     {
         $this->pdo = null;
