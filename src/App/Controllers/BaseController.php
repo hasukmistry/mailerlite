98d9a8b60bc6e5f3ae54583a2df95960121f1741
Hasmukh K Mistry <hasmukhmistry137@gmail.com>
2024-01-28 13:45:44 +0530

    Refactor database initialization in BaseController

--- src/App/Controllers/BaseController.php
+++ src/App/Controllers/BaseController.php
@@ -5,24 +5,24 @@ use App\Database\Connection as Database;
 
 class BaseController
 {
-	/**
-	 * Database instance
-	 *
-	 * @var mixed
-	 */
-	protected $db;
+    /**
+     * Database instance
+     *
+     * @var mixed
+     */
+    protected $db;
 
-	/**
-	 * Create a new BaseController instance
-	 *
-	 */
-	public function __construct()
-	{
-		$this->db = new Database(
-			getenv('MYSQL_HOST'),
-			getenv('MYSQL_DATABASE'),
-			getenv('MYSQL_USER'),
-			getenv('MYSQL_PASSWORD')
-		);
-	}
+    /**
+     * Create a new BaseController instance
+     *
+     */
+    public function __construct()
+    {
+        $this->db = new Database(
+            getenv('MYSQL_HOST'),
+            getenv('MYSQL_DATABASE'),
+            getenv('MYSQL_USER'),
+            getenv('MYSQL_PASSWORD')
+        );
+    }
 }
