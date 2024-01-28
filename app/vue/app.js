fc28f7cde5f6b6b104ce82a7d6f37af5f464a275
Hasmukh K Mistry <hasmukhmistry137@gmail.com>
2024-01-28 14:03:05 +0530

    Add import for app.scss and update request interceptor and api url generator

--- app/vue/app.js
+++ app/vue/app.js
@@ -1,6 +1,6 @@
+import '@sass/app.scss';
 import Vue from 'vue';
 import App from '@/App.vue';
-import '@sass/app.scss';
 import store from '@/store/index';
 import router from '@/router';
 import superagent from 'superagent';
@@ -10,15 +10,19 @@ Vue.use(VeeValidate);
 
 const request = superagent.agent();
 
+// request interceptor
 request.use((req) => {
 	req.url = `${process.env.VUE_APP_API_HOST || 'http://localhost:8000'}/${req.url}`;
 	return req;
 });
 
+// api url generator
 Vue.prototype.$urls = {};
 Vue.prototype.$urls.api = function (url) {
 	return 'mailerlite/v1/' + url;
 };
+
+// http request
 Vue.prototype.$http = request;
 
 new Vue({
